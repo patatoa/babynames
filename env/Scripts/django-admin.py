d33fc2212038431426a3e49366285cb349e5c6ce
patatoa <patatoa@gmail.com>
2013-07-22 08:49:53 -0500

    Initial checkin

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\developer1\Dropbox\Documents\projects\babyname_site\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
