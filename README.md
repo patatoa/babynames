a45411b834e03227617fd4f28854447994d69b19
patatoa <patatoa@gmail.com>
2013-07-22 09:09:51 -0500

    Wrote out readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 babynames
 =========
 
-babynames
+This project is a simple django web app for me and my wife to use as we brainstorm and collaborate upon names for our baby.
---
+++ env/Scripts/fab-script.py
@@ -0,0 +1,9 @@
+#!C:\Users\developer1\Dropbox\Documents\projects\babyname_site\env\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'Fabric==1.6.1','console_scripts','fab'
+__requires__ = 'Fabric==1.6.1'
+import sys
+from pkg_resources import load_entry_point
+
+sys.exit(
+   load_entry_point('Fabric==1.6.1', 'console_scripts', 'fab')()
+)
