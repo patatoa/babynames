530f759b8dc6826e008d4cd5ea121200215022cd
patatoa <patatoa@gmail.com>
2013-07-23 17:00:48 -0500

    Tried fiddling with admin, butno avail

--- babynames/babynames_app/admin.py
+++ babynames/babynames_app/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
-from babynames.babynames_app.models import FirstName, LastName
-
-class FirstAdmin(admin.ModelAdmin):
-	pass
-admin.site.register(FirstName, FirstAdmin)
+from babynames_app.models import FirstName, LastName, FullName, Country, Gender
 
 
-class LastAdmin(admin.ModelAdmin):
-	pass
-admin.site.register(LastName, LastAdmin)
+class Choice(admin.ModelAdmin):
+	exclude = ('rank',)
+
+
+class FirstNameInlines(admin.StackedInline):
+	model= FirstName 
+	extra = 50
+
+
+class FullNameAdmin(admin.ModelAdmin):
+	inlines = [FirstNameInlines]
+
+
+admin.site.register(FirstName, Choice)
+admin.site.register(FullName, FullNameAdmin)
+admin.site.register(Country)
+admin.site.register(Gender)
