18d1ba339b0437ef3f351145de6c35e781f5c2d3
Patrick <patatoa>
2013-08-03 13:10:52 -0500

    Please let me save my models! Q_Q

--- babynames/babynames_app/models.py
+++ babynames/babynames_app/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
 # Create your models here.
-#tgtrgfrn
-
 
 class LastName(models.Model):
 	lastName = models.CharField(max_length = 20)
@@ -12,39 +10,50 @@ class LastName(models.Model):
 		return self.lastName
 
 
+class FirstName(models.Model):
+    name = models.CharField(max_length = 25)
+    nicknames = models.ManyToManyField('FirstName')
+
+
+    def __unicode__(self):
+        return self.name
+
+
 class Gender(models.Model):
 	genderAccronym = models.CharField(max_length = 1)
 	genderLabel = models.CharField(max_length = 13)
-	
-	
+
+
 	def __unicode__(self):
 		return self.genderAccronym
 
 
 class Country(models.Model):
 	countryName = models.CharField(max_length = 35)
-	
-	
+
+
 	def __unicode__(self):
 		return self.countryName
 
 
-class FirstName (models.Model):
-	firstName = models.CharField(max_length = 20)
-	country = models.ForeignKey('Country')
-	rank = models.IntegerField(default = 0)
+class NameInfo (models.Model):
+	firstName = models.ForeignKeyField(FirstName)
+	country = models.ForeignKey('Country', null = True)
 	gender = models.ForeignKey('Gender')
-	meaning = models.CharField(max_length = 100)
-	
-	
+	meaning = models.CharField(max_length = 100, null=True)
+
+
 	def __unicode__(self):
-		return self.firstName
+		return "%s" % (self.firstName)
 
 
 class FullName(models.Model):
 	first = models.ForeignKey(FirstName)
 	last = models.ForeignKey(LastName)
-	
-	
+	rank =  models.IntegerField(default = 0)
+    pros = models.CharField(max_length - 100, null = True)
+    cons = models.CharField(max_length - 100, null = True)
+
+
 	def __unicode__(self):
 		return '%s %s' % (self.first.firstName, self.last.lastName)
